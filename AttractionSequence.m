745e2b297f009fa8373cdfa3fd37d109af7a1ea6
MichaelaRabenius <michaela.rabenius@gmail.com>
2018-05-14 11:24:07 +0200

    Attraction sequence

---
+++ AttractionSequence.m
@@ -0,0 +1,13 @@
+function [] = AttractionSequence(seq, attractions)
+%ATTRACTIONSEQUENCE Summary of this function goes here
+%   Detailed explanation goes here
+evaluated_seq = eval(seq);
+
+for i = 1:length(evaluated_seq)
+    st = strcat(num2str(evaluated_seq(i,1)), " ", num2str(evaluated_seq(i,2)));
+    ride = attractions(strcmp(attractions.pos, st),:)
+    
+end
+
+end
+
