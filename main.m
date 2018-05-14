e0ef26829ffaffac3dd649e9e5abe4853ea0e68b
yrsa <yrsa.lifvergren@gmail.com>
2018-05-14 08:11:34 +0200

    made a path per person

--- main.m
+++ main.m
@@ -2,9 +2,9 @@
 %Timestamp (%{yyyy-MM-dd HH:mm:ss})D, id(%d), type(%C), X(%d), Y(%d)
 % formatSpec = '%{yyyy-MM-dd HH:mm:ss}D%d%C%d%d';
 % 
-% load 'MatlabData/friday.mat';
-% load 'MatlabData/saturday.mat';
-% load 'MatlabData/sunday.mat';
+load 'MatlabData/friday.mat';
+load 'MatlabData/saturday.mat';
+load 'MatlabData/sunday.mat';
 
 %% load park image
 parkmap = imread('Auxiliary Files/Park Map.jpg');
@@ -23,14 +23,82 @@ c = sortedM(p.type == 'check-in',:);
 
 d = M(M.type == 'check-in',:);
 sortedD = sortrows(d, 4);
-nrAttractions = unique(sortedD.(4));
+%nrAttractions = unique(sortedD.(4));        %not very accurate because we only look at one coord
 
+A = table2array(sortedD(:,4:5));        % An array only with the locations checked-in
+nrA = unique(A);            % number of attractions
+
+%% Histogram, did not turn out as I expected
 histogram(sortedD.(4))
 hold on
 histogram(sortedD.(5))
 hold off
 
-%histogram2(sortedD.(4), sortedD.(5))
+%histogram(A(:))        %% does it do what we want? Want to look at both X and Y, not only one
+histogram2(sortedD.(4), sortedD.(5))
+
+%% Lets find groups     <-- Does not work
+[MRows Mcols] = size(sortedM); 
+prevID = sortedM.(2)(1);
+positions(1,1) = [sortedM.(4)(1) sortedM.(5)(1)]; 
+
+% put in this person in the first slot
+
+% Go through all persons in a day
+for i = 2:MRows
+    tempID = sortedM.(2)(i);
+    thisPosition = [sortedM.(4)(i) sortedM.(5)(i)]
+    
+    if tempID == prevID
+        %put in next col
+        positions(i,1) = [positions, thisPosition]
+        
+    else % sortedM.(2)(is) ~= tempID        % this is same person
+        %put in next row
+    end
+    
+    prevID = tempID
+end
+
+% if .type == 'check-in' --> store in a vector
+    % as a string like in lab 3?
+
+% check if another person has the same vector
+
+%% New less messy try
+
+% Get all IDs in the data
+Ids = unique(sortedM.(2));
+[nrIds ~] = size(Ids);
+T = table;
+Ttemp = table;
+
+% loop through the ids, one at a time
+for i = 1:nrIds
+    %positions = [];
+    positions = "";
+    n = sortedM(sortedM.id == Ids(i),:);
+    [nsize, ~] = size(n);
+     
+    % get all positions for id and put them in a table
+    for j = 1:nsize
+        
+        npos = [n.(4)(j) n.(5)(j)];
+        
+        positions = positions + npos;
+    end
+    
+    Ttemp.id = Ids(i); %n.(2)(i);
+    Ttemp.visitedAttractions = positions;
+    T = [T;Ttemp];
+end
+
+%% Lets use the data
+sortedT = sortrows(T, 2);
+nrGroups = unique(sortedT.(2));
+
+%% Next step:
+% Make a table with all the groups. group number and a string of ids?
 
 
 %%
