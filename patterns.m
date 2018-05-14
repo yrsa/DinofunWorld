9732cfbff11467639241275b6aed19e8e3c97101
MichaelaRabenius <michaela.rabenius@gmail.com>
2018-05-14 13:42:05 +0200

    started with checking popularities with ride

--- patterns.m
+++ patterns.m
@@ -9,24 +9,28 @@ load 'fri_groups.mat';
 load 'sat_groups.mat';
 load 'sun_groups.mat';
 
+%%
+groups = [fri_groups; sat_groups; sun_groups];
+
+
 %% Find all large groups with more than 12 people
 
-large_group_fri = FindLargeGroup(fri_groups, 12);
-large_group_sat = FindLargeGroup(sat_groups, 12);
-large_group_sun = FindLargeGroup(sun_groups, 12);
+% large_group_fri = FindLargeGroup(fri_groups, 12);
+% large_group_sat = FindLargeGroup(sat_groups, 12);
+% large_group_sun = FindLargeGroup(sun_groups, 12);
 
 %%
-
-w = large_group_fri.(1);
-e = w(1,1);
-ids = e{1}.id;
-ids = cell2mat(ids);
-cl = friday(friday.id == ids(1),:);
-
-seq = e{1}.sequence;
-seq = seq{1};
-
-AttractionSequence(seq, attractions)
+% 
+% w = large_group_fri.(1);
+% e = w(1,1);
+% ids = e{1}.id;
+% ids = cell2mat(ids);
+% cl = friday(friday.id == ids(1),:);
+% 
+% seq = e{1}.sequence;
+% seq = seq{1};
+% 
+% AttractionSequence(seq, attractions)
 
 
 
