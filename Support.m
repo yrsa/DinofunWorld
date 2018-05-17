3e503fad33276d67b5f72d7e4208f54be3ed1469
MichaelaRabenius <michaela.rabenius@gmail.com>
2018-05-17 14:16:55 +0200

    workinf

--- Support.m
+++ Support.m
@@ -1,24 +1,26 @@
-function [support] = Support(item,seq_table)
+function [times_visited, support] = Support(item, seq_table)
 %SUPPORT Summary of this function goes here
 %   Detailed explanation goes here
 load 'attractions.mat';
 
-pos = attractions(strcmp(attractions.name, item));
+pos = attractions(strcmp(attractions.name, item),:);
 pos = table2array(pos(1, 3:4));
 
 support = 0;
+times_visited = 0;
 for i=1:height(seq_table) 
     row = seq_table(i,:);
-    seq = row.seq(1);
+    seq = row.sequence(1);
+    seq = seq{1};
     seq = eval(seq);
     
     [r ~] = size(seq);
     for j=1:r
         if(seq(j,:) == pos)
-            support = support + 1;
+            times_visited = times_visited + 1;
         end
     end
 end
 
-support = support / height(seq_table);
+support = times_visited / height(seq_table);
 
--- test.m
+++ test.m
@@ -2,4 +2,15 @@
 load 'MatlabData/friday.mat';
 
 smalldata = friday(1:1000,:);
-Apriori(smalldata);
+
+% data = friday;
+% 
+% ids = data.id;
+% ids = unique(ids);
+% nr_ids = length(ids);
+% 
+% %% Create a sequence table
+% seq_table = CreateSeqTable(ids,data);
+
+%%
+e = Apriori(seq_table);
