431cca4dcd72d940dd08d84b9d896513fd189e2c
MichaelaRabenius <michaela.rabenius@gmail.com>
2018-05-17 13:28:52 +0200

    working on support

--- Apriori.m
+++ Apriori.m
@@ -1,47 +1,29 @@
-function [itemsets] = Apriori(seq_table)
-%Apriori algorithm to find frequently occuring sequences in the
-%data
+function [] = Apriori(data)
+%APRIORI Summary of this function goes here
+%   Detailed explanation goes here
 
+%% find unique number of people
+ids = data.id;
+ids = unique(ids);
+nr_ids = length(ids);
+
+%% Create a sequence table
+seq_table = CreateSeqTable(ids,data);
+
+%% Define the itemset (ride names)
 load attractions.mat;
-itemsets = attractions(:,5);
-times_visited = zeros(height(itemsets), 1);
-times_visited = array2table(times_visited);
-itemsets = [itemsets times_visited];
-
-size(itemsets)
-
-%for every person visiting the park
-for k = 1:height(seq_table)
-    person = seq_table(k,:);
-    seq = person.sequence{1};
-    seq = eval(seq);
-    %find out how many people has visited each ride.
-    for i=1:height(itemsets)
-        a = attractions(i,3:5);
-        pos = table2array(a(:,1:2));
-        
-        [row ~] = size(seq);
-        for j=1:row
-            
-            if(seq(j,:) == pos)
-                val = itemsets.times_visited(i);
-                 itemsets.times_visited(i) = val +1;
-            end
-        end
-    end
+itemsets1 = attractions.name;
+itemsets1 = array2table(itemsets1);
+itemsets1.Properties.VariableNames = {'name'};
+%% calculate the support for each item.
+support = zeros(height(itemsets1),1); % array to store supports
+for i=1:height(itemsets1)
+    item = itemsets1.name(i);
+    support(i) = Support(item, seq_table);
 end
 
-% calclulate the support for each item.
-itemsets = Support(height(seq_table), itemsets);
+% add the suport to the table
+support = array2table(support);
 
-% Remove the items that are not interesting enough according to a threshold
-threshold = 0.5; %50
-to_remove = itemsets.support < threshold;
-itemsets(to_remove, :) = [];
-
-% now compute the different combinations of items
-% config = ComputeConfiguration(itemsets);
-
-
-end
+itemsets1 = [itemsets1 support];
 
